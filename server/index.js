1fdf13ae7adc2c20476decee0ad618a5fe1a4f21
bubao <asd565586630@gmail.com>
2019-08-24 23:46:12 +0800

    Headers

--- server/index.js
+++ server/index.js
@@ -16,10 +16,16 @@ app.use(BodyParser.urlencoded({ extended: true }));
 
 // Allow cross-origin requests.
 app.all('*', (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method == "OPTIONS") {
+    res.send(200);
+  }
+  else {
+    next();
+  }
 });
 
 // Set up the /build route.
