3b2f81e8a722cdd1485f98ca8a859afc4ec4a257
bubao <asd565586630@gmail.com>
2019-08-26 01:51:35 +0800

    test

--- server/index.js
+++ server/index.js
@@ -20,12 +20,12 @@ app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
   res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method == "OPTIONS") {
-    res.send(200);
-  }
-  else {
-    next();
-  }
+//   if (req.method == "OPTIONS") {
+//     res.send(200);
+//   }
+//   else {
+//     next();
+//   }
 });
 
 // Set up the /build route.
