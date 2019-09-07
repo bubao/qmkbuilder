5f8fc5769774228383bd70b2b0a1580c23a6eec7
bubao <asd565586630@gmail.com>
2019-09-08 04:48:10 +0800

    zip

--- src/ui/panes/compile/index.js
+++ src/ui/panes/compile/index.js
@@ -122,22 +122,22 @@ class Compile extends React.Component {
       .send(JSON.stringify({package:1,files}))
       .end((err, res) => {
         // Download the hex file.
-        res = JSON.parse(res.text)
+        // res = JSON.parse(res.text)
 
-        if (err) {
-          console.error(err)
-          state.error('Unable to connect to API server.')
-          state.ui.set('compile-working', false)
-          return
-        }
+        // if (err) {
+        //   console.error(err)
+        //   state.error('Unable to connect to API server.')
+        //   state.ui.set('compile-working', false)
+        //   return
+        // }
 
-        // Check if there was an error.
-        if (res.error) {
-          console.error(res.error)
-          state.error('Server error:\n' + res.error)
-          state.ui.set('compile-working', false)
-          return
-        }
+        // // Check if there was an error.
+        // if (res.error) {
+        //   console.error(res.error)
+        //   state.error('Server error:\n' + res.error)
+        //   state.ui.set('compile-working', false)
+        //   return
+        // }
 
         // Generate a friendly name.
         const friendly = keyboard.settings.name
