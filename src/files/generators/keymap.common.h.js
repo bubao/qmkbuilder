cf6e9ee3bd2e4b058bc9ab005473000e3a67f0f2
bubao <asd565586630@gmail.com>
2019-09-08 15:05:00 +0800

    分离

--- src/files/generators/keymap.common.h.js
+++ src/files/generators/keymap.common.h.js
@@ -1,4 +1,5 @@
 const Generator = require('./index')
+const Utils = require('utils')
 
 const C = require('const')
 
@@ -11,122 +12,73 @@ class KeymapC extends Generator {
     const keyboard = this.keyboard
 
     // Generate the keymaps.
-    let keymaps = ''
-    for (let layer = 0; layer < C.KEYMAP_MAX_LAYERS; layer++) {
-      let layerMap = '\tKEYMAP(\n\t\t'
+    const keymap1 = (() => {
+      const rowLength = String(keyboard.rows).length
+      const colLength = String(keyboard.cols).length
+      const space = ' '.repeat(rowLength + colLength + 3)
+
+      let result = ''
+
       for (let row = 0; row < keyboard.rows; row++) {
+        const sRow = Utils.leftPad(String(row), rowLength, '0')
+
+        let rowString = ''
         for (let col = 0; col < keyboard.cols; col++) {
-          const key = keyboard.wiring[row + ',' + col]
-          if (!key || !key.length) continue
+          const sCol = Utils.leftPad(String(col), colLength, '0')
 
-          layerMap += key[0].keycodes[layer].getCode() + ', '
+          if (keyboard.wiring[row + ',' + col]) {
+            rowString += 'K' + sRow + sCol + ', '
+          } else {
+            rowString += space
+          }
         }
-        layerMap += '\n\t\t'
+
+        if (row === keyboard.rows - 1) {
+          rowString = rowString.replace(/,[ ]*?$/, ', ')
+        }
+        result += '\t' + rowString + '\\\n'
       }
-      layerMap = layerMap.substring(0, layerMap.length - 5) + '),\n\n'
-      keymaps += layerMap
-    }
-    keymaps = keymaps.substring(0, keymaps.length - 3)
 
-    // Generate the macros.
-    // let macros = '';
-    // for (const macroId in keyboard.macros) {
-    // 	macros += '\t\tcase ' + macroId + ':\n\t\t\tif (record->event.pressed) {\n\t\t\t\treturn MACRO( ';
+      result = result.substring(0, result.length - 4) + '  \\'
 
-    // 	for (const action of keyboard.macros[macroId]) {
-    // 		switch (action.action) {
-    // 			case C.MACRO_NONE: continue;
-    // 			case C.MACRO_INTERVAL: macros += 'I('; break;
-    // 			case C.MACRO_DOWN: macros += 'D('; break;
-    // 			case C.MACRO_UP: macros += 'U('; break;
-    // 			case C.MACRO_TYPE: macros += 'T('; break;
-    // 			case C.MACRO_WAIT: macros += 'W('; break;
-    // 		}
-    // 		switch (action.action) {
-    // 			case C.MACRO_INTERVAL:
-    // 			case C.MACRO_WAIT:
-    // 				macros += action.argument;
-    // 				break;
-    // 			case C.MACRO_DOWN:
-    // 			case C.MACRO_UP:
-    // 			case C.MACRO_TYPE:
-    // 				macros += action.argument.substring(3);
-    // 				break;
-    // 		}
-    // 		macros += '), ';
-    // 	}
+      return result
+    })()
 
-    // 	macros += 'END );\n\t\t\t}\n\t\t\tbreak;\n';
-    // }
-    // macros = macros.substring(0, macros.length - 1);
+    const keymap2 = (() => {
+      const rowLength = String(keyboard.rows).length
+      const colLength = String(keyboard.cols).length
+      const cellLength = Math.max(rowLength + colLength + 1, 'KC_NO'.length) + 2
+
+      let result = ''
+
+      for (let row = 0; row < keyboard.rows; row++) {
+        const sRow = Utils.leftPad(String(row), rowLength, '0')
+
+        let rowString = ''
+        for (let col = 0; col < keyboard.cols; col++) {
+          const sCol = Utils.leftPad(String(col), colLength, '0')
+
+          if (keyboard.wiring[row + ',' + col]) {
+            rowString += Utils.rightPad('K' + sRow + sCol + ', ', cellLength)
+          } else {
+            rowString += Utils.rightPad('KC_NO' + ', ', cellLength)
+          }
+        }
+
+        rowString = rowString.trim()
+        result +=
+          '\t{ ' + rowString.substring(0, rowString.length - 1) + ' }, \\\n'
+      }
+
+      result = result.substring(0, result.length - 4) + '  \\'
+
+      return result
+    })()
 
     return {
-      keymaps: keymaps,
-      // 'macros': macros,
-      quantum: keyboard.quantum,
-      led_on_num: this.generateLedOn(keyboard.pins.num),
-      led_off_num: this.generateLedOff(keyboard.pins.num),
-      led_on_caps: this.generateLedOn(keyboard.pins.caps),
-      led_off_caps: this.generateLedOff(keyboard.pins.caps),
-      led_on_scroll: this.generateLedOn(keyboard.pins.scroll),
-      led_off_scroll: this.generateLedOff(keyboard.pins.scroll),
-      led_on_compose: this.generateLedOn(keyboard.pins.compose),
-      led_off_compose: this.generateLedOff(keyboard.pins.compose),
-      led_on_kana: this.generateLedOn(keyboard.pins.kana),
-      led_off_kana: this.generateLedOff(keyboard.pins.kana)
+      keymap_1: keymap1,
+      keymap_2: keymap2
     }
-  }
-
-  /*
-   * Generate the code to turn an LED on given a pin.
-   *
-   * @param {String} pin The pin.
-   *
-   * @return {String} The generated code.
-   */
-  generateLedOn(pin) {
-    if (!pin) return ''
-
-    const port = pin[0]
-    const num = pin[1]
-
-    return (
-      'DDR' +
-      port +
-      ' |= (1 << ' +
-      num +
-      '); PORT' +
-      port +
-      ' &= ~(1 << ' +
-      num +
-      ');'
-    )
-  }
-
-  /*
-   * Generate the code to turn an LED off given a pin.
-   *
-   * @param {String} pin The pin.
-   *
-   * @return {String} The generated code.
-   */
-  generateLedOff(pin) {
-    if (!pin) return ''
-
-    const port = pin[0]
-    const num = pin[1]
-
-    return (
-      'DDR' +
-      port +
-      ' &= ~(1 << ' +
-      num +
-      '); PORT' +
-      port +
-      ' &= ~(1 << ' +
-      num +
-      ');'
-    )
   }
 }
 
