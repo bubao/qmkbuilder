29a4d1a08d40d1c61759cce8f23b77d538c3fa00
bubao <asd565586630@gmail.com>
2019-09-08 14:49:57 +0800

    分离

--- src/files/index.js
+++ src/files/index.js
@@ -2,22 +2,22 @@ const ConfigHGenerator = require('./generators/config.h')
 const KeyPlainCGenerator = require('./generators/key_plain.c')
 const KeymapCommonHGenerator = require('./generators/keymap.common.h')
 class Files {
-
-	/*
-	 * Generate the set of source files given a Keyboard.
-	 *
-	 * @param {Keyboard} keyboard The keyboard to generate files from.
-	 *
-	 * @return {Object} The generated source files.
-	 */
-	static generate(keyboard) {
-		return {
-			'tmk_firmware/config.h': new ConfigHGenerator(keyboard).generate(),
-			'tmk_firmware/keymap_plain.c': new KeyPlainCGenerator(keyboard).generate(),
-			// 'ble60/keymap.common.h': new KeymapCommonHGenerator(keyboard).generate()
-		};
-	}
-
+  /*
+   * Generate the set of source files given a Keyboard.
+   *
+   * @param {Keyboard} keyboard The keyboard to generate files from.
+   *
+   * @return {Object} The generated source files.
+   */
+  static generate(keyboard) {
+    return {
+      'tmk_firmware/config.h': new ConfigHGenerator(keyboard).generate(),
+      'tmk_firmware/keymap_plain.c': new KeyPlainCGenerator(
+        keyboard
+      ).generate(),
+      'ble60/keymap_common.h': new KeymapCommonHGenerator(keyboard).generate()
+    }
+  }
 }
 
-module.exports = Files;
+module.exports = Files
