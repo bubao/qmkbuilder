5b8e0284c7a327ec1a584f6e8b9a76c57e676447
bubao <asd565586630@gmail.com>
2019-09-22 02:29:30 +0800

    log

--- src/files/generators/key_plain.c.js
+++ src/files/generators/key_plain.c.js
@@ -21,7 +21,10 @@ class KeyboardH extends Generator {
 				for (let col = 0; col < keyboard.cols; col++) {
 					const key = keyboard.wiring[row + ',' + col]
 					if (!key || !key.length) continue
-					if (key[0].keycodes[layer].getCode().indexOf('MO(') === 0) {
+					console.log(
+						key[0].keycodes[layer].getCode().indexOf('MO') === 0
+					)
+					if (key[0].keycodes[layer].getCode().indexOf('MO') === 0) {
 						console.log('FN' + FNnum)
 						layerMap += 'FN' + FNnum + ', '
 						FNnum += 1
--- src/files/generators/templates/keymap_plain.c.js
+++ src/files/generators/templates/keymap_plain.c.js
@@ -25,7 +25,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const action_t fn_actions[] = {
     /* Poker Layout */
     // ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
-    %FNarr%,
+    %fn%,
     ACTION_FUNCTION(POWER_SLEEP), // sleep
     ACTION_FUNCTION(SWITCH_USB) // switch between usb and ble
 };
