ec1401719454045cdc93a866201a9838d40741a2
Geno [格诺] <genokolar@gmail.com>
2019-09-22 21:39:53 +0800

    调整keycode

--- src/const/keycodes.js
+++ src/const/keycodes.js
@@ -427,13 +427,13 @@ const keycodes = {
 		[]
 	),
 	'TO()': new Keycode(
-	new Template(['TO', 'LAYER'], 'TO(%1)'),
-		'ACTION_LAYER_TOGGLE(%1)',
+	new Template(['TO', 'LAYER'], 'ACTION_LAYER_TOGGLE(%1)'),
+		'TO(%1)',
 		[]
 	),
 	'MO()': new Keycode(
-		new Template(['MO', 'LAYER'], 'MO(%1)'),
-		'ACTION_LAYER_MOMENTARY(%1)',
+		new Template(['MO', 'LAYER'], 'ACTION_LAYER_MOMENTARY(%1)'),
+		'MO(%1)',
 		[]
 	),
 	'DF()': new Keycode(new Template(['DF', 'LAYER'], 'DF(%1)'), 'DF(%1)', []),
