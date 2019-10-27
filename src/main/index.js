c84fbec66e143baec9f2faf03c21380316967c1b
Geno [格诺] <genokolar@gmail.com>
2019-10-27 21:04:25 +0800

    Update index.js

--- src/main/index.js
+++ src/main/index.js
@@ -26,7 +26,7 @@ class Index extends React.Component {
 
 		return <div>
 			<div className='header'>
-				Lot Keyboard Builder
+			<a href="http://glab.online" target="_blank"><img src="images/logo.png"></a>
 			</div>
 			<Screen
 				state={ this.state }/>
