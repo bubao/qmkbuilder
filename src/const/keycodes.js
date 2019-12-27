af4e7aaa19e5b258ebcc9d770a5b1d4df7f4b282
Geno [格诺] <genokolar@gmail.com>
2019-12-27 21:16:58 +0800

    添加鼠标按键

--- src/const/keycodes.js
+++ src/const/keycodes.js
@@ -722,7 +722,24 @@ const categories = {
 		'KC_WFWD',
 		'KC_WSTP',
 		'KC_WREF',
-		'KC_WFAV'
+		'KC_WFAV',
+		'',
+
+		'KC_MS_U',
+		'KC_MS_D',
+		'KC_MS_L',
+		'KC_MS_R',
+		'KC_BTN1',
+		'KC_BTN2',
+		'KC_BTN3',
+		'KC_BTN4',
+		'KC_BTN5',
+		'KC_WH_U',
+		'KC_WH_D',
+		'KC_WH_L',
+		'KC_WH_R',
+		'KC_ACL0',
+		'KC_ACL1'
 	],
 
 	KEYPAD: [
