ec1acbf19bb26bbfa292369cf8fa19a5edee357f
Geno [格诺] <genokolar@gmail.com>
2019-12-30 22:32:59 +0800

    更新所有预设文件

--- src/const/presets.js
+++ src/const/presets.js
@@ -1,5 +1,6 @@
 module.exports = {
-	'gt-ble60': 'GT_BLE60',
+	'ble60d': 'GT_BLE60 rev.D',
+	'ble60e': 'GT_BLE60 rev.E',
 	'farad69': 'Farad69',
 	'omega45': 'Omega45（Ω45）',
 	'omega50': 'Omega50（Ω50）'
