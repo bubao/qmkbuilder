350074b1b3190fa33854d80ab1b173f0d703ca54
Geno [格诺] <genokolar@gmail.com>
2020-01-09 11:00:40 +0800

    屏蔽ble60 rev.E

--- src/const/presets.js
+++ src/const/presets.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'ble60d': 'GT_BLE60 rev.D',
-	'ble60e': 'GT_BLE60 rev.E',
+	//'ble60e': 'GT_BLE60 rev.E',
 	'farad69': 'Farad69',
 	'omega45': 'Omega45（Ω45）',
 	'omega50': 'Omega50（Ω50）'
