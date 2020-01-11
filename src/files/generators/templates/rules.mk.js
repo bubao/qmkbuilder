6329bb1fbde8d357fe222ef40dc37c1f388c97c3
Geno [格诺] <genokolar@gmail.com>
2020-01-11 13:37:49 +0800

    重新启用六键无冲

--- src/files/generators/templates/rules.mk.js
+++ src/files/generators/templates/rules.mk.js
@@ -8,7 +8,7 @@ NRF_CHIP := %nrf_chip%
 # BOOTMAGIC_ENABLE = yes	# 启用Bootmagic
 EXTRAKEY_ENABLE = yes	    # 启用媒体键功能
 NKRO_ENABLE = yes	        # 启用USB的全键无冲功能
-# USB_6KRO_ENABLE = yes     # 启用USB的六键无冲功能
+USB_6KRO_ENABLE = yes     # 启用USB的六键无冲功能
 COMMAND_ENABLE = yes        # 启用调试和配置的命令
 ifeq (nrf52832,$(strip $(NRF_CHIP)))
 %rgblight_enable%RGBLIGHT_ENABLE = yes       #启用RGB灯
