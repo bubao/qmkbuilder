b6c8bd3485fd9f5235dec4552dc8758337220ab8
Geno [格诺] <genokolar@gmail.com>
2020-01-17 20:24:28 +0800

    显示升级公告2

--- src/main/main.js
+++ src/main/main.js
@@ -114,7 +114,8 @@ class Main extends React.Component {
 		const state = this.props.state;
 
 		return <div>
-			<h6>使用此网站前，请确认您升级了新的USB固件：<a href="https://glab.online/download" target="_blank">下载新的USB固件</a></h6>
+			<h6>使用此网站前，请确认您升级了新的USB固件：</h6>
+			<h6><a href="https://glab.online/download" target="_blank">下载新的USB固件</a></h6>
 			<br/><br/>
 			<h3>选择一个预设的按键布局</h3>
 			{(() => {
